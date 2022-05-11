9048d528e0c0b491800b506f11fa3677dae23e9c
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2022-05-12 00:18:00 +0700

    Form Trang chu va Sinh Vien

--- QLSV/GiaoDienChinh.cs
+++ QLSV/GiaoDienChinh.cs
@@ -12,9 +12,35 @@ namespace QLSV
 {
     public partial class GiaoDienChinh : Form
     {
+        private Form currentFormChild;
         public GiaoDienChinh()
         {
             InitializeComponent();
+        }
+        private void OpenChildForm(Form childForm)
+        {
+            if(this.currentFormChild != null)
+            {
+                currentFormChild.Close();
+            }
+            this.currentFormChild = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panel.Controls.Add(childForm);
+            panel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+
+        }
+        private void btnTrangchu_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Trangchu());
+        }
+
+        private void btnSinhvien_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Sinhvien());
         }
     }
 }
