195a7fe0e30a2b9293d0db641db3627436a6be0d
CaoHuynhBaoAn1001 <caoan0012345@gmail.com>
2022-05-12 14:19:51 +0700

    test

--- QLSV/Form1.Designer.cs
+++ QLSV/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace QLSV
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(155, 54);
+            this.button1.Location = new System.Drawing.Point(514, 251);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(274, 187);
             this.button1.TabIndex = 0;
