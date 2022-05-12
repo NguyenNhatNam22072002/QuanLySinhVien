743132af77156db7ed060c7761231cfeab337646
CaoHuynhBaoAn1001 <caoan0012345@gmail.com>
2022-05-12 15:15:21 +0700

    Them procedure Select all lop

--- Query_QLSV_CK.sql
+++ Query_QLSV_CK.sql
@@ -579,4 +579,12 @@ end
 -- kiểm tra --
 SuaThongTinMonHoc 'STMA240121', N'Sức bền vật liệu', '3'
 select * from MonHoc
+-- thêm procedure Lấy thông tin bảng lớp--
+create proc Show_DSlop
+as
+begin 
+select * from Lop
+end 
+go
+
 
